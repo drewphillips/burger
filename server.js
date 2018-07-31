4c2af60905264d467ef79750adeba75db29a3beb
drewphillips <drew@superheroprinting.com>
2018-07-31 16:39:54 -0700

    files, folders and json packages

---
+++ server.js
@@ -0,0 +1,22 @@
+
+var mysql = require('mysql');
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "drew",
+  password: "password1",
+  database: "bamazon"
+})
+
+connection.connect(function (err) {
+  if (err) throw err;
+
+  console.log("connected!")
+}
+  
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+  };
