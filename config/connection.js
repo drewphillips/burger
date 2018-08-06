687e0815d3b4e3117c5886bb11376ffee0a77f18
drwphillips <drew@superheroprinting.com>
2018-08-06 13:15:56 -0700

    new connection fix

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,16 @@ var mysql = require("mysql");
 var connection;
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else 
+} else {
 
-(connection = mysql.createConnection)({
+connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "drew",
     password: "password1",
     database: "burger_db"
   })
+}
 
   connection.connect(function(err) {
     if (err) {
